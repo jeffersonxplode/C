7fa5cd036958ecc3138e8dd7901ea7064b17f748
Jefferson <jeffersonxplode@gmail.com>
2014-11-16 16:22:28 -0300

    Arrumado
    Questão da forma certa agora :D.

--- Lista5C/Q1/program.c
+++ Lista5C/Q1/program.c
@@ -1,51 +1,48 @@
 #include <stdio.h>
-
-int gera_divisores(unsigned long int num, FILE * arquivo){ 
- 
-	int i = 1;
-	
-	fprintf(arquivo, "Divisores de %ld: \n", num);
-	
-	for(i;i<=num;i++){
-		
-		if(num % i == 0){
-			
-			fprintf(arquivo, "%d \n", i);
-		
-		}
-	}
-
-	
-	return 0; 
-
-}
+#include "divisores.h"
 
 
-int main(int argc, char **argv)
-{
-	
-	unsigned long int num;
-	FILE * arq_divisores;
-	
-	arq_divisores = fopen("divisores.txt","w"); 
-	
-	if (arq_divisores== NULL){ 
-		printf("ERROR: Não foi possível abrir o arquivo.\n"); 
+void usage(){ 
+	printf("\n\n Uso do programa gera_divisores:\n\n"); 
+	printf(" ./gera_divisores NUM ARQ\n\n"); 
+	printf(" onde NUM é um inteiro com o número cujo divisores se deseja gerar\n"); 
+	printf(" e ARQ é o nome do arquivo a ser criado com os divisores de NUM."); 
+	printf("TODOS os parâmetros são obrigatórios"); 
+
+} 
+int check_parameters(int argc, char **argv){ 
+
+	return 1; 
+
+} 
+unsigned long int converte(char *num){ 
+
+	unsigned long int n = atoi(num);
+
+	return n; 
+} 
+int main(int argc, char * argv[]) { 
+	FILE * arq_divisores; 
+	if (!check_parameters(argc, argv)){ 
+		usage(); 
+		return 1; 
+	} 
+
+	arq_divisores = fopen(argv[2],"w"); 
+
+	if (arq_divisores== NULL){  
+		printf("ERROR: Não foi possível abrir o arquivo %s.\n",argv[2]); 
 		return 2; 
 	} 
-	
-	scanf("%ld",&num);
-	
-	gera_divisores(num, arq_divisores);
-	
-	if (gera_divisores(num,arq_divisores) != 0){ 
-		printf("ERROR: Problemas ao escrever dados no arquivo. \n"); 
+
+	if (gera_divisores(converte(argv[1]),arq_divisores) != 0){ 
+		printf("ERROR: Problemas ao escrever dados no arquivo %s.\n", argv[2]); 
 		return 3; 
-	}
-	
-	
-	
-	fclose(arq_divisores); 	
-	return 0;
+	} 
+
+	gera_divisores (converte(argv[1]),arq_divisores);
+
+	fclose(arq_divisores); 
+	return 0; 
 }
 
